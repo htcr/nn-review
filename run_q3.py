183f5ea106517d4d37f081f61b50db5c516d5242
htcr <congruihetang@gmail.com>
2018-11-21 13:13:22 -0500

    added a bunch of plotting

--- run_q3.py
+++ run_q3.py
@@ -1,12 +1,18 @@
 import numpy as np
 import scipy.io
 from nn import *
+import matplotlib.pyplot as plt
+import pickle
+
+'''
 
 train_data = scipy.io.loadmat('../data/nist36_train.mat')
 valid_data = scipy.io.loadmat('../data/nist36_valid.mat')
+test_data = scipy.io.loadmat('../data/nist36_test.mat')
 
 train_x, train_y = train_data['train_data'], train_data['train_labels']
 valid_x, valid_y = valid_data['valid_data'], valid_data['valid_labels']
+test_x, test_y = test_data['test_data'], test_data['test_labels']
 
 max_iters = 100
 # pick a batch size, learning rate
@@ -22,6 +28,11 @@ params = {}
 # initialize layers here
 initialize_weights(1024, hidden_size, params, 'layer1')
 initialize_weights(hidden_size, 36, params, 'output')
+
+epochs = list()
+losses = list()
+train_accs = list()
+val_accs = list()
 
 # with default settings, you should get loss < 150 and accuracy > 80%
 for itr in range(max_iters):
@@ -62,24 +73,76 @@ for itr in range(max_iters):
     valid_loss, valid_acc = compute_loss_and_acc(valid_y, probs)
     print('Validation accuracy: ', valid_acc)
 
+    epochs.append(itr)
+    train_accs.append(total_acc)
+    val_accs.append(valid_acc)
+    losses.append(total_loss)
+
+h1 = forward(test_x, params,'layer1')
+test_probs = forward(h1, params,'output',softmax)
+test_loss, test_acc = compute_loss_and_acc(test_y, test_probs)
+print('test accuracy: ', test_acc)
+
+plt.subplot(2, 1, 1)
+plt.plot(epochs, train_accs)
+plt.plot(epochs, val_accs)
+plt.ylabel('train/val accuracy')
+
+plt.subplot(2, 1, 2)
+plt.plot(epochs, losses)
+plt.ylabel('loss')
+
+plt.xlabel('epochs')
+
+plt.show()
+
 if False: # view the data
     for crop in xb:
-        import matplotlib.pyplot as plt
         plt.imshow(crop.reshape(32,32).T)
         plt.show()
-import pickle
+
 saved_params = {k:v for k,v in params.items() if '_' not in k}
 with open('q3_weights.pickle', 'wb') as handle:
     pickle.dump(saved_params, handle, protocol=pickle.HIGHEST_PROTOCOL)
+
+'''
 
 # Q3.1.3
 import matplotlib.pyplot as plt
 from mpl_toolkits.axes_grid1 import ImageGrid
 
+saved_params = None
+with open('q3_weights.pickle', 'rb') as handle:
+    saved_params = pickle.load(handle)
 
+weight_tensor = saved_params['Wlayer1']
+
+fig = plt.figure()
+grid = ImageGrid(fig, 111, (8, 8))
+for i in range(64):
+    filter_i = weight_tensor[:, i]
+    filter_i = filter_i.reshape(32, 32)
+    grid[i].imshow(filter_i)
+    grid[i].axis('off')
+
+plt.show()
+
+initialize_weights(1024, 64, saved_params, 'layer1raw')
+
+weight_tensor = saved_params['Wlayer1raw']
+
+fig = plt.figure()
+grid = ImageGrid(fig, 111, (8, 8))
+for i in range(64):
+    filter_i = weight_tensor[:, i]
+    filter_i = filter_i.reshape(32, 32)
+    grid[i].imshow(filter_i)
+    grid[i].axis('off')
+
+plt.show()
 
 # Q3.1.3
-confusion_matrix = np.zeros((train_y.shape[1],train_y.shape[1]))
+confusion_matrix = np.zeros((36, 36))
 
 import string
 plt.imshow(confusion_matrix,interpolation='nearest')
