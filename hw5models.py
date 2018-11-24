30b624872507532c04903bb2c573b8c7376cb582
htcr <congruihetang@gmail.com>
2018-11-24 15:12:46 -0500

    added lenet and mnist train

--- hw5models.py
+++ hw5models.py
@@ -14,6 +14,25 @@ class NaiveNet(nn.Module):
         x = self.fc2(x)
         return x
 
+class LeNet(nn.Module):
+    def __init__(self):
+        super(LeNet, self).__init__()
+        self.conv1 = nn.Conv2d(1, 8, 5)
+        self.conv2 = nn.Conv2d(8, 16, 5)
+        self.fc1   = nn.Linear(16*4*4, 128)
+        self.fc2   = nn.Linear(128, 64)
+        self.fc3   = nn.Linear(64, 10)
+
+    def forward(self, x):
+        out = F.relu(self.conv1(x))
+        out = F.max_pool2d(out, 2)
+        out = F.relu(self.conv2(out))
+        out = F.max_pool2d(out, 2)
+        out = out.view(out.size(0), -1)
+        out = F.relu(self.fc1(out))
+        out = F.relu(self.fc2(out))
+        out = self.fc3(out)
+        return out
 
 class BigNet(nn.Module):
     def __init__(self):
@@ -34,3 +53,4 @@ class BigNet(nn.Module):
         out = F.relu(self.fc2(out))
         out = self.fc3(out)
         return out
+
